1959b1bd3136b4407d38b19b5c1b44930d328ffc
mjlee0509 <mjlee0509@gmail.com>
2023-08-01 11:12:48 +0900

    01. React 프로젝트 초기 세팅 - Index.html / App.js / Index.js만 남겨두기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// "root"라는 div를 react-dom의 최상위 요소로
+const RootElement = document.getElementById("root")
+const root = ReactDOM.createRoot(RootElement);
+
+// 하위 요소 렌더링
 root.render(
-  <React.StrictMode>
+  // React.StrictMode : 디버깅 도구. 지금 당장 필요하진 않을듯
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
