0394626d5ebfd18984f23e5fd897819f6d0bb57b
mjlee0509 <mjlee0509@gmail.com>
2023-09-04 14:21:51 +0900

    05-1. 왜 useState를 사용하는가? - state가 아닌 일반 변수로 계수기 만들기

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,11 +3,20 @@ import { useState } from 'react'
 
 export default function Counter() {
 
-  const [count, setCount] = useState(0);
+//     // useState 초기값을 0으로 지정
+//     const [count, setCount] = useState(0);
 
-  const updateCount = (arg) => {
-    setCount (count + arg);
-  } 
+//     // 버튼 클릭시 이벤트 함수 정의
+//   const updateCount = (arg) => {
+//     setCount (count + arg);
+//   } 
+
+    // 일반 변수로 state를 다뤄보자
+    let count = 0;
+    const updateCount = (arg) => {
+        count = count + arg;
+    }
+
 
 
   return (
